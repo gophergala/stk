f22fbcc0a02cd010d574193f59471e37a7a0a405
Igor Vasilcovsky <vasilcovsky@gmail.com>
2015-01-24 14:50:11 -0500

    code for output formatting

--- stk.go
+++ stk.go
@@ -1,8 +1,36 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	result, _ := Search("drush failed")
+	/*result, _ := Search("drush failed")
 	for _, item := range result.Items {
 		println(item.Title)
-	}
+	}*/
+
+	//printExplanation("TEst", "https://script.fail")
+
+	printError("Error occured", "Something went wrong", "http://script.fail")
+
+}
+
+func printError(errstr string, maybeReason string, detailUrl string) {
+	fmt.Println(errstr)
+	fmt.Println()
+	fmt.Println(bold("Possible reason:"))
+	fmt.Println(maybeReason)
+	fmt.Println()
+	fmt.Println(bold("Details: "))
+	fmt.Println(underline(detailUrl))
+	fmt.Println()
+}
+
+func bold(text string) string {
+	return "\033[1m" + text + "\033[0m"
+}
+
+func underline(text string) string {
+	return "\033[4m" + text + "\033[0m"
 }
