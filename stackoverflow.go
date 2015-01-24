dc42c15e4e7f79588bdd79261776a5c06e6cb983
Igor Vasilcovsky <vasilcovsky@gmail.com>
2015-01-24 11:44:08 -0500

    Implemented functions Search/GetAnswers

--- stackoverflow.go
+++ stackoverflow.go
@@ -3,58 +3,127 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 )
 
-const (
-	ADVANCED_SEARCH_URL = "https://api.stackexchange.com/2.2/search/advanced?"
-)
-
-type Item struct {
-	link        string `json:"link"`
-	title       string `json:"title"`
-	question_id int64  `json:"int"`
+type Quota struct {
+	QuotaMax       int `json:"quota_max"`
+	QuotaRemaining int `json:"quota_remaining"`
 }
 
 type SearchResult struct {
-	items           []Item `json:"items"`
-	quota_max       int    `json:"quota_max"`
-	quota_remaining int    `json:"quota_remaining"`
+	Items []struct {
+		Tags  []string `json:"tags"`
+		Owner struct {
+			Reputation   int    `json:"reputation"`
+			UserId       int    `json:"user_id"`
+			UserType     string `json:"user_type"`
+			ProfileImage string `json:"profile_image"`
+			DisplayName  string `json:"display_name"`
+			Link         string `json:"link"`
+		} `json:"owner"`
+		IsAnswered       bool   `json:"is_answered"`
+		ViewCount        int    `json:"view_count"`
+		AnswerCount      int    `json:"answer_count"`
+		AcceptedAnswerId int    `json:"accepted_answer_id"`
+		Score            int    `json:"score"`
+		LastActivityDate int    `json:"last_activity_date"`
+		CreationDate     int    `json:"creation_date"`
+		QuestionId       int    `json:"question_id"`
+		Link             string `json:"link"`
+		Title            string `json:"title"`
+	} `json:"items"`
+	HasMore bool `json:"has_more"`
+	Quota
 }
 
-func Search(query string) {
+type AnswerResponse struct {
+	Items []struct {
+		Owner struct {
+			Reputation   int    `json:"reputation"`
+			UserId       int    `json:"user_id"`
+			UserType     string `json:"user_type"`
+			AcceptRate   int    `json:"accept_rate"`
+			ProfileImage string `json:"profile_image"`
+			DisplayName  string `json:"display_name"`
+			Link         string `json:"link"`
+		} `json:"owner"`
+		IsAccepted       bool   `json:"is_accepted"`
+		Score            int    `json:"score"`
+		LastActivityDate int    `json:"last_activity_date"`
+		CreationDate     int    `json:"creation_date"`
+		AnswerId         int    `json:"answer_id"`
+		QuestionId       int    `json:"question_id"`
+		Body             string `json:"body"`
+	} `json:"items"`
+	HasMore bool `json:"has_more"`
+	Quota
+}
+
+func makeSearchRequest(query string) string {
 	args := url.Values{}
 	args.Set("order", "desc")
 	args.Set("sort", "activity")
 	args.Set("site", "stackoverflow")
+	args.Set("accepted", "True")
 	args.Set("q", query)
 
-	url := ADVANCED_SEARCH_URL + args.Encode()
-	log.Printf(url)
+	return "https://api.stackexchange.com/2.2/search/advanced?" + args.Encode()
+}
 
-	resp, err := http.Get(url)
+func makeAnswerRequest(answerIds ...int) string {
+	var ids = make([]string, len(answerIds))
+
+	for i, val := range answerIds {
+		ids[i] = strconv.Itoa(val)
+	}
+
+	escaped := url.QueryEscape(strings.Join(ids, ";"))
+
+	args := url.Values{}
+	args.Set("order", "desc")
+	args.Set("sort", "activity")
+	args.Set("filter", "withbody")
+
+	return "https://api.stackexchange.com/2.2/answers/" + escaped + "?" + args.Encode()
+}
+
+func get(url string) (body []byte, err error) {
+	res, err := http.Get(url)
+	defer res.Body.Close()
+
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	return ioutil.ReadAll(res.Body)
+}
+
+func Search(query string) (result *SearchResult, err error) {
+	url := makeSearchRequest(query)
+	body, err := get(url)
+
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	var dat map[string]interface{}
+	result = new(SearchResult)
+	err = json.Unmarshal(body, &result)
+	return
+}
 
-	if err := json.Unmarshal(body, &dat); err != nil {
-		panic(err)
+func GetAnswers(ids ...int) (result *AnswerResponse, err error) {
+	url := makeAnswerRequest(ids...)
+	body, err := get(url)
+
+	if err != nil {
+		return nil, err
 	}
 
-	for k, v := range dat {
-		println(k)
-		println(v)
-	}
-
-	//println(string(body))
+	result = new(AnswerResponse)
+	err = json.Unmarshal(body, &result)
+	return
 }
