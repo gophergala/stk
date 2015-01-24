9992849bbe4e574ca77507df32afe19e90e0ac2c
Brian Oluwo <broluwo@umd.edu>
2015-01-24 15:24:36 -0500

    Cleaning it up a bit

--- stk/main.go
+++ stk/main.go
@@ -11,6 +11,17 @@ import (
 //Program receives a string of the command that is supposed to be run.
 //ie stk go run execTest.go
 
+//LaunchReq is a struct that represents everything that
+//os.StartProcess is being provided
+type LaunchReq struct {
+	//The name of the command which we will find the path of
+	//and eventually fill out the rest of the data
+	Base     string
+	Path     string
+	Argv     []string
+	ProcAttr os.ProcAttr
+}
+
 //If we end up needing something a bit closer to the metal,
 //look at os.StartProcess before getting hackish
 
@@ -30,18 +41,34 @@ func init() {
 // 4. Get results, prepend file name to whatever the output was from the api
 func main() {
 	//	cmd := cleanInput(os.Args[1:])
-	//This will choke if to cmd are passed at once,ie
-	cmd := cleanInput(os.Args[1:]...)
-	execCmd(&cmd)
+	//This will choke if more than one cmd is passed
+	cmd, err := cleanInput(os.Args[1:]...)
+	if err != nil {
+		log.Fatalf("The provided command is not installed: %T %v",
+			err,
+			err)
+	}
+	execCmd(cmd)
 }
 
 //CleanInput takes all the relevant arguments from os.Args
 //and tries to break it down into an exec.Cmd struct
 //This will need a lot of tuning as it will be fragile
-func cleanInput(arg ...string) exec.Cmd {
+func cleanInput(arg ...string) (lr *LaunchReq, err error) {
+	if len(arg) <= 0 {
+		log.Fatalln("Must provide input.")
+	}
 	fmt.Printf("Args: %v\n", arg)
-	//might use os.StartProcess to avoid the
-	return exec.Cmd{}
+	lr = &LaunchReq{Base: os.Args[1], ProcAttr: os.ProcAttr{}}
+	lr.Path, err = exec.LookPath(lr.Base)
+	if err != nil {
+		return
+	}
+	lr.Argv = os.Args[1:]
+	fmt.Printf("%T %#v", lr.Argv, lr.Argv)
+	//This is where we redirect the io.Writer
+	lr.ProcAttr.Files = []*os.File{nil, os.Stdout, os.Stderr}
+	return
 }
 
 //This is going to be the main event loop in all actuality
@@ -49,23 +76,16 @@ func cleanInput(arg ...string) exec.Cmd {
 //blocking on the exit of the cmd
 //Redirects the stderr(which expects an io.Writer) into a channel,
 //which the API is blocking on in order to launch a request.
-func execCmd(cmd *exec.Cmd) {
-	//Make sure the actual command exists, ie go
-	path, err := exec.LookPath(os.Args[1])
-	if err != nil {
-		log.Fatalf("Bloody hell, at least install the command: %s\n",
-			os.Args[1])
-	}
-	fmt.Printf("Yay?%s\n", path)
-	var procAttr os.ProcAttr
-	procAttr.Files = []*os.File{nil, os.Stdout, os.Stderr}
-	process, e := os.StartProcess(path, os.Args[1:], &procAttr)
+func execCmd(cmd *LaunchReq) {
+	process, e := os.StartProcess(cmd.Path, os.Args[1:], &cmd.ProcAttr)
 	if e != nil {
 		log.Fatal("Process Start Failed", e)
 	}
-	procState, errs := process.Wait()
+	var procState *os.ProcessState
+	defer log.Printf("Process Exited: %#v", procState)
+	procState, e = process.Wait()
 
-	if errs != nil {
-		log.Fatal("Err on Exit:", procState, errs)
+	if e != nil {
+		log.Fatal("Err on Exit:", procState, e)
 	}
 }
